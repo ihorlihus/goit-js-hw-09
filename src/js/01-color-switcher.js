d37e08c8a0353d092e692245d5c59ec2489752bd
ihorlihus <ligusigor@ukr.net>
2022-06-16 16:27:33 +0300

    task 1.

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const startButtonsEl = document.querySelector('button[data-start]');
+const stopButtonsEl = document.querySelector('button[data-stop]');
 
+startButtonsEl.addEventListener('click', createBgColor);
+stopButtonsEl.addEventListener('click', stopColorChange);
+stopButtonsEl.setAttribute('disabled', true);
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+function checkActive() {
+    if (createBgColor) {
+        startButtonsEl.setAttribute('disabled', true);
+    }
+};
+
+function createBgColor() {
+        timerId = setInterval(() => {
+        const randomColor = getRandomHexColor();
+        document.body.style.backgroundColor = randomColor;
+    }, 1000);
+    checkActive();
+    stopButtonsEl.removeAttribute('disabled', true);
+};
+
+function stopColorChange() {
+    startButtonsEl.removeAttribute('disabled', true);
+    stopButtonsEl.setAttribute('disabled', true);
+    clearInterval(timerId);
+    console.log('Stoped')
+};
